09798de5af49bfa654f51ce841a6eed2d4d585bb
therealpeppy <furghadmanar@gmail.com>
2025-03-16 03:28:35 -0500

    changed ts to js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 window.onload = function () {
-    const hiddenItems = document.getElementsByClassName("hidden") as HTMLCollectionOf<HTMLElement>;
+    const hiddenItems = document.getElementsByClassName("hidden");
     for (let i = 0; i < hiddenItems.length; i++) {
         hiddenItems[i].remove();
     }
     const backgrounds = ['src/assets/Assets/bg.jpg', 'src/assets/Assets/bg3.jpg', 'src/assets/Assets/bg4.jpg'];
     const randomBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 
-    const backgroundElement = document.querySelector('.background') as HTMLElement | null;
+    const backgroundElement = document.querySelector('.background');
     if (backgroundElement) {
         backgroundElement.style.backgroundImage = `url('${randomBg}')`;
         backgroundElement.style.opacity = "0.8";
