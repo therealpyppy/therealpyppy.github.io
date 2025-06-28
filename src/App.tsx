4e07ed093c8f52fca66b0ca8e2c022c3cb2496d1
therealpeppy <furghadmanar@gmail.com>
2025-06-28 15:44:43 -0500

    add mode changer

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import Navigation from './components/Navigation'
 import Home from './pages/Home'
 import Term from './pages/Term'
 import Trad from './pages/Traditional'
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
     return (
-        <BrowserRouter>
-        <Navigation />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/terminal" element={<Term />} />
-                <Route path="/traditional" element={<Trad />} />
-            </Routes>
-        </BrowserRouter>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <BrowserRouter>
+                <Navigation />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/terminal" element={<Term />} />
+                    <Route path="/traditional" element={<Trad />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
     )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,22 @@
 import { Link } from 'react-router-dom'
+import { ModeToggle } from '@/components/mode-toggle'
 
 export default function Home() {
     return (
-        <div>
-            <Link to="/terminal">
-                <h1>
-                    Click here to go to the terminal page
-                </h1>
-            </Link>
-            <br></br>
-            <Link to="/traditional">
-                <h1>
-                    Click here to go to the traditional page
-                </h1>
-            </Link>
-            <br></br>
+        <div className="min-h-screen grid place-items-center">
+            <div className="flex flex-col items-center gap-8">
+                <ModeToggle />
+                <Link to="/terminal">
+                    <h1 className="text-xl font-semibold text-center">
+                        Click here to go to the terminal page
+                    </h1>
+                </Link>
+                <Link to="/traditional">
+                    <h1 className="text-xl font-semibold text-center">
+                        Click here to go to the traditional page
+                    </h1>
+                </Link>
+            </div>
         </div>
     )
 }
