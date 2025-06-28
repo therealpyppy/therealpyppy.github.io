359c91f064b07cf16be05978fea4a2fe99be0364
therealpeppy <furghadmanar@gmail.com>
2025-06-28 15:53:07 -0500

    make home button appear over elements instead of above

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Navigation() {
     return (
-        <nav>
+        <nav className="fixed top-2 left-2 z-50">
             <Button asChild size="sm">
                 <Link to="/">
                     Home
