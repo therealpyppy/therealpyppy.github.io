b309f49a400ed7409a987503406b6a751b54e88b
therealpeppy <furghadmanar@gmail.com>
2025-07-01 19:54:05 -0500

    fix incorrect navigation text underline

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -1,15 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function ProfileHeader() {
+    const location = useLocation();
     return (
         <div className="absolute top-2 right-2 z-50 flex flex-row gap-4">
             <Link to="/traditional">
-                <h1 className="text-xl font-semibold text-right underline text-white">
+                <h1
+                    className={`text-xl font-semibold text-right text-white ${
+                        location.pathname === '/traditional' ? 'underline' : ''
+                    }`}
+                >
                     About
                 </h1>
             </Link>
             <Link to="/traditional/projects">
-                <h1 className="text-xl font-semibold text-right text-white">
+                <h1
+                    className={`text-xl font-semibold text-right text-white ${
+                        location.pathname === '/traditional/projects' ? 'underline' : ''
+                    }`}
+                >
                     Projects
                 </h1>
             </Link>
